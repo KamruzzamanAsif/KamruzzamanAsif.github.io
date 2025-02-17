913d4e54e0fb2d46a7754d1e51f0ee77096b8fcf
KamruzzamanAsif <asif720kamruzzaman@gmail.com>
2025-02-17 13:06:49 +0600

    cv link added

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -22,6 +22,7 @@ export const aboutMe: AboutMe = {
   name: "Kamruzzaman Asif",
   title: "Software Engineering Undergrad",
   institution: "University of Dhaka",
+
   // Note that links work in the description
   description:
     "I'm a final-year <a href='http://www.iit.du.ac.bd'>software engineering undergrad</a> at University of Dhaka. My research focuses on machine learning, deep learning, and generative AI, specifically addressing web memory leaks in Single-Page Applications. I have interned at <a href='https://selisegroup.com/'>SELISE Digital Platforms</a>, gaining experience in data engineering and machine learning solutions. My technical skills include Python, PySpark, SQL, and various libraries and frameworks. I have achieved recognition in competitions like <b>BUET DL Sprint 3.0</b> and <b>ASR for Regional Dialects, Bengali.AI x IUT</b>.",
@@ -34,7 +35,7 @@ export const aboutMe: AboutMe = {
   linkedinUsername: "kamruzzaman-asif",
   // twitterUsername: "janesmith",
   // blogUrl: "https://",
-  // cvUrl: "https://",
+  cvUrl: "https://docs.google.com/document/d/e/2PACX-1vRVDtYJhCjpVUWSBWEetBH9vtYjmIaCv8-JbKT0uxi8Brhf5lo0_kB7N1FiP-ih7YZ0oVAk1yw62uQ4/pub",
   institutionUrl: "http://www.iit.du.ac.bd",
   // altName: "",
   // secretDescription: "I like dogs.",
